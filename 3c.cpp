69f55c8af2f734102bbc4b30a8d810a4018ad921
Adit2002 <95736811+Adit2002@users.noreply.github.com>
2022-05-29 15:40:47 +0530

    Update 3c.cpp

--- 3c.cpp
+++ 3c.cpp
@@ -1,20 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 bool isValid(string s) {
-        stack<char> st;
+        stack<char>A;
         for(int i=0;i<s.size();i++){
             if(s[i]=='(' or s[i]=='{' or s[i]=='['){
-                st.push(s[i]);
+                A.push(s[i]);
             }
             else{
-                if(st.empty()) return false;
-                else if(st.top()=='(' and  s[i]!=')') return false;
-                else if(st.top()=='{' and  s[i]!='}') return false;
-                else if(st.top()=='[' and  s[i]!=']') return false;
-                else st.pop();
+                if(A.empty())
+                return false;
+                else if(A.top()=='(' and  s[i]!=')') 
+                return false;
+                else if(A.top()=='{' and  s[i]!='}')
+                return false;
+                else if(A.top()=='[' and  s[i]!=']')
+                return false;
+                else A.pop();
             }
         }
-        if(st.size()>0) return false;
+        if(A.size()>0) return false;
         return true;
     }
 int main()
